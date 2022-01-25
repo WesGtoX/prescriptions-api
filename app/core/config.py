4862e82c1afb914331585e38315c82a921cbbf7c
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-01-25 00:08:33 -0300

    feat: adding new environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,21 @@
-import ast
-
-from typing import Dict
 from pydantic import BaseSettings
 from decouple import config
 
-# def env_to_dict(env: str) -> Dict[Any, Any]:
-#     return json.loads(env)
-# import ast
-# my_dict = ast.literal_eval(TESTING_BROWSERS)
-
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = config('DATABASE_URL', default='')
+    SQLALCHEMY_DATABASE_URL: str = config('DATABASE_URL', default='postgres://prescription:prescription@db:5432/prescription')
+    REDIS_URL: str = config('REDIS_URL', default='redis://localhost')
+
     DEPENDENT_SERVICES_URL: str = config('DEPENDENT_SERVICES_URL', default='')
-    DEPENDENT_SERVICES_AUTH: Dict[str, str] = ast.literal_eval(config('DEPENDENT_SERVICES_AUTH', default='{}'))
+
+    PHYSICIANS_AUTH: str = config('PHYSICIANS_AUTH', default='')
+    CLINICS_AUTH: str = config('CLINICS_AUTH', default='')
+    PATIENTS_AUTH: str = config('PATIENTS_AUTH', default='')
+    METRICS_AUTH: str = config('METRICS_AUTH', default='')
+
+    TTL_PHYSICIAN: int = config('TTL_PHYSICIAN', default=172800)  # 48h
+    TTL_CLINIC: int = config('TTL_CLINIC', default=259200)  # 72h
+    TTL_PATIENT: int = config('TTL_PATIENT', default=43200)  # 12h
 
     class Config:
         env_file = '.env'
