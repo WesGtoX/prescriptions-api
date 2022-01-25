249eb3cfc59d0535c3716666a065d7178e95da81
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-01-25 00:00:15 -0300

    feat: adding redis startup and shutdown event

--- app/db/database.py
+++ app/db/database.py
@@ -8,7 +8,7 @@ from app.core.config import settings
 uri = settings.SQLALCHEMY_DATABASE_URL
 SQLALCHEMY_DATABASE_URL = uri.replace('postgres://', 'postgresql://', 1) if uri.startswith('postgres://') else uri
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from typing import Dict
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, Request, status
 from sqlalchemy.orm import Session
 
 from app import crud, schemas, dependencies, docs
 from app.db.database import Base, engine
+from app.db.redis import get_redis_pool
 
 Base.metadata.create_all(bind=engine)
 
@@ -12,13 +13,24 @@ app = FastAPI(
     description='API to insert new prescriptions.',
 )
 
+@app.on_event('startup')
+async def startup_event() -> None:
+    app.state.redis = await get_redis_pool()
+
+
+@app.on_event('shutdown')
+async def shutdown_event() -> None:
+    await app.state.redis.close()
+
 
 @app.get('/', responses={200: docs.RESPONSES_EXAMPLE.get('hello_world')}, tags=['hello_world'])
 def hello_world() -> Dict[str, str]:
     return {'hello_world': 'Prescriptions API'}
 
 
-@app.post('/prescriptions/', response_model=schemas.Prescription, status_code=status.HTTP_201_CREATED, tags=['prescriptions'])
-async def create_prescription(prescription: schemas.PrescriptionCreate, db: Session = Depends(dependencies.get_db)) -> schemas.Prescription:
+@app.post('/prescriptions/', response_model=schemas.Prescription,
+          status_code=status.HTTP_201_CREATED, tags=['prescriptions'])
+async def create_prescription(request: Request, prescription: schemas.PrescriptionCreate,
+                              db: Session = Depends(dependencies.get_db)) -> schemas.Prescription:
     db_prescription = crud.Prescription(db=db)
-    return await db_prescription.process(prescription=prescription)
+    return await db_prescription.process(request=request, prescription=prescription)
